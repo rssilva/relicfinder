23714f1ca18a320f146f8f5c6e837414a8c11733
Rafael Specht <rafaelspdasilva@gmail.com>
2022-09-15 19:45:26 -0300

    splitting code but in a ugly way

--- utils/parseDataByModule.js
+++ utils/parseDataByModule.js
@@ -14,7 +14,7 @@ const isSpecifierMissing = (moduleImportedData) => (specifier) =>
   !moduleImportedData.usedItems.some((item) => _.isEqual(item, specifier))
 
 const getSpecifiers = (imports = [], sourcePath) =>
-  imports.find(hasSamePath(sourcePath)).specifiers || []
+  imports.find(hasSamePath(sourcePath))?.specifiers || []
 
 const getUniqSpecifiers = (specifiers, moduleImportedData) =>
   _.uniq(
@@ -26,14 +26,50 @@ const getUniqSpecifiers = (specifiers, moduleImportedData) =>
 const getExportAll = (exports, modulesData, extensions) =>
   exports.filter(isExportAll).map(getModulePath(modulesData, extensions)).flat()
 
+const isInternalModule = (moduleData) => !moduleData.isExternal
+
 const dataTemplate = { usedItems: [], usedBy: [] }
 
+const handleImports = ({
+  importData,
+  modulesData,
+  importedItemsByFile,
+  filePath,
+  currentModule,
+  moduleFileName,
+}) => {
+  const importSourcePath = importData._sourcePath
+
+  if (!importedItemsByFile[moduleFileName]) {
+    importedItemsByFile[moduleFileName] = { ...dataTemplate }
+  }
+
+  const moduleImportedData = importedItemsByFile[moduleFileName]
+
+  moduleImportedData.usedBy = moduleImportedData.usedBy.concat(filePath)
+
+  const specifiers = getSpecifiers(currentModule?.imports, importSourcePath)
+
+  if (specifiers.length) {
+    // get items that are used by any other module uniquely
+    // TODO: maybe does not repeat to count how many times is used?
+    moduleImportedData.usedItems = getUniqSpecifiers(
+      specifiers,
+      moduleImportedData
+    )
+  }
+
+  if (!modulesData[moduleFileName]) {
+    // console.log('no data associated with', filePath, importData._sourcePath)
+  }
+}
+
 const parseDataByModule = ({ modulesData, extensions }) => {
-  let allImports = []
   const importedItemsByFile = {}
+  let allImports = []
 
   Object.entries(modulesData).forEach(([filePath, currentModule]) => {
-    const importedByCurrent = currentModule.imports.filter((i) => !i.isExternal)
+    const importedByCurrent = currentModule.imports.filter(isInternalModule)
     const exportAll = getExportAll(
       currentModule.exports,
       modulesData,
@@ -44,32 +80,40 @@ const parseDataByModule = ({ modulesData, extensions }) => {
 
     importedByCurrent.forEach((importData) => {
       const moduleFileName = getModulePath(modulesData, extensions)(importData)
-      const importSourcePath = importData._sourcePath
-
-      if (!importedItemsByFile[moduleFileName]) {
-        importedItemsByFile[moduleFileName] = { ...dataTemplate }
-      }
-
-      const moduleImportedData = importedItemsByFile[moduleFileName]
-
-      moduleImportedData.usedBy = moduleImportedData.usedBy.concat(filePath)
-
-      const specifiers = getSpecifiers(currentModule?.imports, importSourcePath)
-
-      if (specifiers.length) {
-        // get items that are used by any other module uniquely
-        // TODO: maybe does not repeat to count how many times is used?
-        moduleImportedData.usedItems = getUniqSpecifiers(
-          specifiers,
-          moduleImportedData
-        )
-      }
 
       moduleFileName ? allImports.push(moduleFileName) : null
 
-      if (!modulesData[moduleFileName]) {
-        // console.log('does not have module data associated with', filePath, importData._sourcePath)
-      }
+      handleImports({
+        importData,
+        modulesData,
+        importedItemsByFile,
+        filePath,
+        currentModule,
+        moduleFileName,
+      })
+    })
+
+    exportAll.forEach((exportFilePath) => {
+      // if (!importedItemsByFile[exportFilePath]) {
+      //   importedItemsByFile[exportFilePath] = { ...dataTemplate }
+      // }
+      // if (!importedItemsByFile[exportFilePath].usedBy.includes(filePath)) {
+      //   console.log(filePath, modulesData[filePath])
+      //   importedItemsByFile[exportFilePath].usedBy.push(filePath)
+      //   importedItemsByFile[exportFilePath].usedItems = importedItemsByFile[
+      //     exportFilePath
+      //   ].usedItems.concat(
+      //     getSpecifiers(
+      //       modulesData[filePath].imports,
+      //       modulesData[exportFilePath]._sourcePath
+      //     )
+      //   )
+      // }
+      // console.log(exportFilePath, filePath)
+      // const specifiers =
+      //   getSpecifiers(currentModule?.imports || [], exportFilePath) || []
+      // importedItemsByFile[exportFilePath].usedItems =
+      //   importedItemsByFile[exportFilePath].usedItems.concat(specifiers)
     })
   })
 
