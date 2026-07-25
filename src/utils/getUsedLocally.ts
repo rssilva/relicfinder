e43571e85c20d71a2bc3bcbb03adc7d0422bff6e
Rafael Specht <rafaelspdasilva@gmail.com>
2026-07-25 19:40:50 -0300

    we are going to places

--- src/utils/getUsedLocally.ts
+++ src/utils/getUsedLocally.ts
@@ -5,45 +5,51 @@ import _ from 'lodash'
 
 export const getUsedLocally = (varsByFile: Record<string, string[]>) => {
   const usedLocally: Record<string, string[]> = {}
-  let pathName = ''
+  // let pathName = ''
 
   Object.entries(varsByFile).forEach(([path, vars]) => {
-    pathName = path.replace(/(\/|\.)/g, '_')
+    // pathName = path.replace(/(\/|\.)/g, '_')
     usedLocally[path] = []
     const code = fs.readFileSync(path).toString()
     const ast = parseCode(code)
 
-    const traversed = traverseEnter({ ast, filePath: path })
+    const traversed = traverseEnter({ ast, filePath: path, searchedVars: vars })
 
     traversed.forEach((node) => {
+      const id = _.get(node, 'id.name')
+      const callee = _.get(node, 'expression.callee.name')
       const name = _.get(node, 'name', '')
-      const hasName = vars.some((varName) => varName === name)
+      const generic = id || callee || name
+      const hasName = vars.some((varName) => varName === generic)
 
-      if (name && hasName) {
-        usedLocally[path].push(String(name))
+      // printf(node.isMatch)
+
+      if (generic && hasName) {
+        // @ts-expect-error isMatch
+        if (node.isMatch) {
+          usedLocally[path].push(String(generic))
+        }
       }
-
-      // usedLocally[path] = _.uniq(usedLocally[path])
     })
   })
+
   const count: Record<string, Record<string, number>> = {}
+
   Object.entries(usedLocally).forEach(([path, vars]) => {
     count[path] = {}
     vars.forEach((variableName) => {
-      if (!count[path][variableName]) {
-        count[path][variableName] = 0
-      }
-
       count[path][variableName]++
     })
   })
 
+  // console.log(usedLocally)
+
   Object.entries(usedLocally).forEach(([path, vars]) => {
     vars.forEach((variableName) => {
       if (count[path]?.[variableName] < 2) {
-        usedLocally[path] = usedLocally[path].filter(
-          (varName) => varName !== variableName,
-        )
+        // usedLocally[path] = usedLocally[path].filter(
+        //   (varName) => varName !== variableName,
+        // )
       } else {
         usedLocally[path] = _.uniq(usedLocally[path])
       }
